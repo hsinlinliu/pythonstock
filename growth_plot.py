83baed9140c0b93d459209b523b20db0700966a1
hsinlinliu <62015612+hsinlinliu@users.noreply.github.com>
2020-07-01 17:07:21 +0800

    Update growth_plot.py

--- growth_plot.py
+++ growth_plot.py
@@ -1,7 +1,6 @@
 import seaborn as sns
 from matplotlib.font_manager import FontProperties
 sns.set(font=['sans-serif'])
-# 有關 Matplotlib / Seaborn 視覺化的中文顯示問題請參考 https://reurl.cc/15x6m
 sns.set_style("whitegrid",{"font.sans-serif":['Microsoft JhengHei']})
 plt.figure(figsize=(12,7))
 sns.barplot(x="INDUSTRY_NAME_2", y="AVG_ACC_YOY", data=df_2018_growth.head(10))
